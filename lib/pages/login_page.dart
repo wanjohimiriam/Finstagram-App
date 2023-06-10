dee8ae81130184c074547e4f13714ce3fff90b9a
Dev_wanjohi <92452113+wanjohimiriam@users.noreply.github.com>
2023-06-10 22:07:39 +0300

    Form field

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -9,7 +9,12 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+
   double? dev_height, dev_width;
+  final GlobalKey <FormState> _loginFromKey= GlobalKey<FormState>();
+
+  String? email;
+  String? password;
   @override
   Widget build(BuildContext context) {
     dev_height = MediaQuery.of(context).size.height;
@@ -25,6 +30,7 @@ class _LoginPageState extends State<LoginPage> {
             mainAxisSize: MainAxisSize.max,
             children: [
               _titleWidget(),
+              _LoginForm(),
               _loginButton(),
             ],
           ),
@@ -55,6 +61,38 @@ class _LoginPageState extends State<LoginPage> {
         style: TextStyle(
             color: Colors.white, fontSize: 20, fontWeight: FontWeight.w600),
       ),
+    );
+  }
+
+  Widget _LoginForm(){
+    return Container(
+      height: dev_height! * 0.20,
+      child: Form(
+        key: _loginFromKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            _emailTextForm(),
+          ],
+        )
+        ),
+    );
+  }
+  Widget _emailTextForm(){
+    return TextFormField(
+      decoration: InputDecoration(
+        hintText: "email",
+      ),onSaved: (value) {
+        setState(() {
+          email= value;
+        });
+      },
+      validator: (_value){
+        _value.contains.(RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+"));
+
+      },
     );
   }
 }
